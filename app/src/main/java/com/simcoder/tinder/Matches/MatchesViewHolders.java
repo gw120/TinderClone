56524dc1c3e260265bc01aa0a211444632f15947
gregoryann <32854050+gregoryann@users.noreply.github.com>
2023-03-05 00:01:26 +0000

    Populate Matches RecyclerView

--- app/src/main/java/com/simcoder/tinder/Matches/MatchesViewHolders.java
+++ app/src/main/java/com/simcoder/tinder/Matches/MatchesViewHolders.java
@@ -2,25 +2,29 @@ package com.simcoder.tinder.Matches;
 
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.simcoder.tinder.R;
-
 /**
  * Created by manel on 10/31/2017.
  */
 
 public class MatchesViewHolders extends RecyclerView.ViewHolder implements View.OnClickListener{
-    public TextView mMatchId;
+    public TextView mMatchId, mMatchName;
+    public ImageView mMatchImage;
+   
     public MatchesViewHolders(View itemView) {
         super(itemView);
         itemView.setOnClickListener(this);
 
         mMatchId = (TextView) itemView.findViewById(R.id.Matchid);
+        mMatchName = (TextView) itemView.findViewById(R.id.MatchName);
+
+        mMatchImage = (ImageView) itemView.findViewById(R.id.MatchImage);
     }
 
     @Override
     public void onClick(View view) {
-
     }
 }
